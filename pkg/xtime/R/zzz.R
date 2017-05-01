f2a845ba9de321722a6a3599379db534c12e279c
bodanker (Joshua Ulrich) <josh.m.ulrich@gmail.com>
2017-05-01 13:16:38 +0000

    Add warning that repo has moved to .onAttach()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/xts@878 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/xtime/R/zzz.R
+++ pkg/xtime/R/zzz.R
@@ -3,3 +3,10 @@ if(getRversion() < "2.11.0") {
     structure(xx, class = c("POSIXt", "POSIXct"), tzone = tz) 
 }
 
+.onAttach <- function(libname, pkgname) {
+  repo <- "https://github.com/joshuaulrich/xtime"
+  packageStartupMessage(
+    "WARNING: this package was installed from R-Forge, but development has\n",
+    "moved to GitHub. Please re-install the package using the GitHub repo at:\n",
+    repo, ".")
+}
--- pkg/xtsExtra/R/zzz.R
+++ pkg/xtsExtra/R/zzz.R
@@ -1,4 +1,10 @@
 .onAttach <- function(...){
   # Suggests quantmod to user of OHLC plot.xts
   assign(".QUANTMOD_MESSAGE", TRUE, .GlobalEnv)
+
+  repo <- "https://github.com/joshuaulrich/xtsExtra"
+  packageStartupMessage(
+    "WARNING: this package was installed from R-Forge, but development has\n",
+    "moved to GitHub. Please re-install the package using the GitHub repo at:\n",
+    repo, ".")
 }
