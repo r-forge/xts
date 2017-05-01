0b6163662bed2812253460dff7324ea3b79c4603
bodanker (Joshua Ulrich) <josh.m.ulrich@gmail.com>
2017-05-01 13:14:52 +0000

    Add warning that repo has moved to .onAttach()
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/xts@877 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/xts/R/zzz.R
+++ pkg/xts/R/zzz.R
@@ -21,3 +21,11 @@ if(getRversion() < "2.11.0") {
     .POSIXct <- function(xx, tz = NULL)
     structure(xx, class = c("POSIXt", "POSIXct"), tzone = tz)
 }
+
+.onAttach <- function(libname, pkgname) {
+  repo <- "https://github.com/joshuaulrich/xts"
+  packageStartupMessage(
+    "WARNING: this package was installed from R-Forge, but development has\n",
+    "moved to GitHub. Please re-install the package using the GitHub repo at:\n",
+    repo, ".")
+}
